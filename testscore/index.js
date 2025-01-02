5916055510817714c4335da0bd81751ba4f4b7c5
Agenorwoth Lleyton Adrian <lagenorwoth@gmail.com>
2025-01-02 23:06:00 +0300

    Add files via upload

---
+++ testscore/index.js
@@ -0,0 +1,17 @@
+let testscore = 85;
+let letterGrade;
+
+switch (true) {
+    case testscore >=90:
+        letterGrade = "A";
+        break;
+
+        case testscore >=80:
+            letterGrade = "B";
+            break;
+
+    default:
+        break;
+}
+
+console.log(letterGrade);
